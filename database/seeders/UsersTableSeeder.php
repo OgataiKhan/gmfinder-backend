3d8d5d5d90faefec79bfc47a47e6ecf10a70127f
OgataiKhan <badellino.kevin@gmail.com>
2024-03-02 15:37:24 +0100

    feat: add game_master to user seeder

--- database/seeders/UsersTableSeeder.php
+++ database/seeders/UsersTableSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\User;
+use App\Models\GameMaster;
 
 class UsersTableSeeder extends Seeder
 {
@@ -12,6 +14,26 @@ class UsersTableSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $users = config('users');
+
+        foreach ($users as $userData) {
+            // Create user
+            $newUser = new User();
+            $newUser->name = $userData['name'];
+            $newUser->email = $userData['email'];
+            $newUser->password = bcrypt($userData['password']);
+            $newUser->role = $userData['role'];
+            $newUser->save();
+
+            // Create game master depending on role
+            if ($userData['role'] === 'game_master') {
+                $newGameMaster = new GameMaster();
+                $newGameMaster->user_id = $newUser->id;
+                $newGameMaster->location = $userData['location'];
+                $newGameMaster->game_description = $userData['game_description'];
+                $newGameMaster->max_players = $userData['max_players'];
+                $newGameMaster->save();
+            }
+        }
     }
 }
