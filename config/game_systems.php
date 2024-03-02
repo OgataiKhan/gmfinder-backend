a2349838bd8d2eb1b91de5fd11adc8260059b246
OgataiKhan <badellino.kevin@gmail.com>
2024-03-02 21:06:01 +0100

    feat: game systems table seeder

---
+++ config/game_systems.php
@@ -0,0 +1,44 @@
+<?php
+
+return [
+  [
+    "name" => "Dungeons & Dragons",
+    "description" => "The original fantasy role-playing game where players embark on epic quests and battle mythical creatures in a medieval setting.",
+  ],
+  [
+    "name" => "Call Of Cthulhu",
+    "description" => "A horror role-playing game set in a universe of cosmic fear inspired by H.P. Lovecraft's works, emphasizing mystery and survival.",
+  ],
+  [
+    "name" => "Pathfinder",
+    "description" => "A fantasy RPG that expands on the rules of Dungeons & Dragons 3.5, offering deep character customization and adventure in a rich fantasy world.",
+  ],
+  [
+    "name" => "Apocalypse World",
+    "description" => "A post-apocalyptic role-playing game focusing on narrative, relationships, and scarcity, with players navigating a harsh world rebuilt from the ashes.",
+  ],
+  [
+    "name" => "Vampire: The Masquerade",
+    "description" => "A dark, story-driven RPG where players become vampires, struggling with their inhuman natures, morality, and the politics of the undead societies.",
+  ],
+  [
+    "name" => "Shadowrun",
+    "description" => "A cyberpunk-meets-fantasy RPG set in a future where magic has returned, blending high technology with ancient powers in a dystopian world.",
+  ],
+  [
+    "name" => "Blades In The Dark",
+    "description" => "A game about a crew of daring scoundrels seeking their fortunes on the haunted streets of an industrial-fantasy city.",
+  ],
+  [
+    "name" => "Warhammer Fantasy Roleplay",
+    "description" => "A gritty fantasy RPG set in the Warhammer world, emphasizing perilous adventures in a land beset by war and chaos.",
+  ],
+  [
+    "name" => "Root RPG",
+    "description" => "Based on the board game, Root RPG plunges players into a vibrant woodland realm, amidst a dynamic war of factions, as vagabonds with their own agendas.",
+  ],
+  [
+    "name" => "Fabula Ultima",
+    "description" => "A Final Fantasy-inspired RPG that combines adventure, magic, and strategic combat in a world of high fantasy and epic storytelling.",
+  ],
+];
--- database/seeders/GameSystemTableSeeder.php
+++ database/seeders/GameSystemTableSeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 
+use App\Models\GameSystem;
+use Illuminate\Support\Str;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +14,16 @@ class GameSystemTableSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $game_systems = config('game_systems');
+
+        foreach ($game_systems as $gameSystemData) {
+            $slug = Str::slug($gameSystemData['name']);
+
+            $newGameSystem = new GameSystem();
+            $newGameSystem->name = $gameSystemData['name'];
+            $newGameSystem->description = $gameSystemData['description'];
+            $newGameSystem->slug = $slug;
+            $newGameSystem->save();
+        }
     }
 }
