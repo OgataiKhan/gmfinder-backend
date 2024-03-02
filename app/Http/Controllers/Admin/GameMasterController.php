30bf054c21cf20dff31a3d884714b694fe9f7377
dieromeo <diegoromeo99@icloud.com>
2024-03-02 14:51:54 +0100

    feat: update and edit structure

--- app/Http/Controllers/Admin/GameMasterController.php
+++ app/Http/Controllers/Admin/GameMasterController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\UpdateGameMasterRequest;
+use App\Models\GameMaster;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
@@ -45,17 +47,25 @@ class GameMasterController extends Controller
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(string $id)
+    public function edit(User $user)
     {
-        //
+        $user = Auth::user();
+        $game_master = $user->gameMaster;
+        return view('game_masters.edit', compact('game_master'));
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(UpdateGameMasterRequest $request, User $user)
     {
-        //
+
+        $user = Auth::user();
+        $data = $request->validated();
+
+        $gameMaster = $user->gameMaster;
+        $gameMaster->max_players = $data['max_players'];
+        $gameMaster->update($data);
     }
 
     /**
--- resources/views/game_masters/edit.blade.php
+++ resources/views/game_masters/edit.blade.php
@@ -1,1 +1,14 @@
+@extends('layouts.admin')
+@section('content')
+    <div class="container">
 
+        <form action={{ route('game_master.update', $game_master) }} method="POST" class="d-flex row p-4 text-light"
+            enctype="multipart/form-data">
+            @csrf
+            @method('PUT')
+            <label for="max_players">Insert a players number</label>
+            <input type="number" name="max_players" id="max_players">
+            <button type="submit" class="btn btn-primary ">Submit</button>
+        </form>
+    </div>
+@endsection
