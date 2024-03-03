81cb9a7af32ceff7d16436aabbf441a617af13f6
rebs-code <jack.rebussi@gmail.com>
2024-03-03 13:42:30 +0100

    Update login page styles

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -5,9 +5,9 @@
         <div class="row justify-content-center">
             <div class="col-md-8">
                 <div class="card">
-                    <div class="card-header">{{ __('Login') }}</div>
+                    <div id="login-card-header" class="card-header">{{ __('Login') }}</div>
 
-                    <div class="card-body">
+                    <div id="login-card-body" class="card-body">
                         <form method="POST" action="{{ route('login') }}">
                             @csrf
 
