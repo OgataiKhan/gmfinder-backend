1699cfc0d55ad54fa9262d2e7bcffae3c21ec6fb
rebs-code <jack.rebussi@gmail.com>
2024-03-04 11:46:28 +0100

    Update admin layout and add GM index CSS

--- resources/views/game_masters/index.blade.php
+++ resources/views/game_masters/index.blade.php
@@ -1,12 +1,13 @@
 @extends('layouts.admin')
 @section('content')
     <div class="container py-4">
+        <h1>My Character</h1>
         @if ($user->gameMaster->is_active)
-            <div class="card">
+            <div class="card" id="gm-card">
                 {{-- show user info --}}
                 <div class="card-header">
                     <div class="d-flex justify-content-between">
-                        <p>{{ $user->name }}</p>
+                        <h3>{{ $user->name }}</h3>
                         <div class="d-flex">
                             {{-- edit profile --}}
                             <a href="{{ route('game_master.edit', $user) }}"
@@ -23,30 +24,35 @@
                 </div>
                 {{-- show game master info --}}
                 @if ($user->gameMaster)
-                    <div class="card-body">
+                    <div class="card-body" id="gm-card-body">
                         Here's my game master info
-                    </div>
-                    <div>
 
-                        {{-- show image --}}
-                        @if ($user->gameMaster->profile_img)
-                            <img src="{{ asset('storage/' . $user->gameMaster->profile_img) }}" alt="Game Master Pic">
-                        @else
-                            <img src="https://icons.veryicon.com/png/o/miscellaneous/xdh-font-graphics-library/anonymous-user.png"
-                                alt="Game Master Pic">
-                        @endif
-                    </div>
+                        <div>
+
+                            {{-- show image --}}
+                            @if ($user->gameMaster->profile_img)
+                                <img src="{{ asset('storage/' . $user->gameMaster->profile_img) }}" alt="Game Master Pic">
+                            @else
+                                <img src="https://icons.veryicon.com/png/o/miscellaneous/xdh-font-graphics-library/anonymous-user.png"
+                                    alt="Game Master Pic">
+                            @endif
+                        </div>
                 @endif
             </div>
-        @else
-            {{-- account deleted --}}
-            <div class="alert p-3 d-flex flex-column align-items-center border border-2">
-                <p>Your account has been deleted.</p>
-                <form action="{{ route('logout') }}" method="POST">
-                    @csrf
-                    <button class="btn btn-primary btn-sm">Home Page</button>
-                </form>
-            </div>
-        @endif
+    </div>
+@else
+    {{-- account deleted --}}
+    <div class="alert p-3 d-flex flex-column align-items-center border border-2">
+        <p>Your account has been deleted.</p>
+        <form action="{{ route('logout') }}" method="POST">
+            @csrf
+            <button class="btn btn-primary btn-sm">Home Page</button>
+        </form>
+    </div>
+    @endif
     </div>
 @endsection
+
+@push('styles')
+    <link href="{{ asset('css/gm-index.css') }}" rel="stylesheet">
+@endpush
