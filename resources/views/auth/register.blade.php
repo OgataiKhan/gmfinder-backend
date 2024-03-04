7f2132ef95f6bfc35a554163cd50a40cd48e7f9b
rebs-code <jack.rebussi@gmail.com>
2024-03-04 15:13:57 +0100

    feat: register page CSS

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,13 +1,13 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="container mt-4">
+    <div class="container my-5">
         <div class="row justify-content-center">
             <div class="col-md-8">
-                <div class="card">
-                    <div class="card-header">{{ __('Register') }}</div>
+                <div class="card" id="register-card">
+                    <div class="card-header" id="register-card-header">{{ __('Register') }}</div>
 
-                    <div class="card-body">
+                    <div class="card-body" id="register-card-body">
                         <form method="POST" action="{{ route('register') }}">
                             @csrf
 
@@ -108,3 +108,8 @@
     </div>
     @vite(['resources/js/confirmPassword.js'])
 @endsection
+
+{{-- css --}}
+@push('styles')
+    <link href="{{ asset('css/register.css') }}" rel="stylesheet">
+@endpush
