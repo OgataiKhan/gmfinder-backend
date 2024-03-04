a2db804ac97b62712a6a2d1ee9bb45e217a96abc
AlexChihaia <alexchihaia84@gmail.com>
2024-03-04 19:31:25 +0100

    feat: validation max players

--- app/Http/Requests/StoreGameMasterRequest.php
+++ app/Http/Requests/StoreGameMasterRequest.php
@@ -25,7 +25,7 @@ class StoreGameMasterRequest extends FormRequest
         return [
             'location' => 'required|string|max:100',
             'game_description' => 'required|string|max:1000',
-            'max_players' => 'required|integer|min:1',
+            'max_players' => 'required|integer|min:1|max:127',
             'profile_img' => 'nullable|file|mimes:jpeg,png,jpg|max:2048',
             'game_systems' => 'required|array|min:1',
             'game_systems.*' => 'exists:game_systems,id',
@@ -38,6 +38,7 @@ class StoreGameMasterRequest extends FormRequest
             'game_systems.required' => 'Please select at least one game system.',
             'game_systems.min' => 'Please select at least one game system.',
             'game_systems.*.exists' => 'The selected game system is invalid.',
+            'max_players.max'=>'You can not select more than 127 players',
         ];
     }
 }
--- app/Http/Requests/UpdateGameMasterRequest.php
+++ app/Http/Requests/UpdateGameMasterRequest.php
@@ -25,7 +25,7 @@ class UpdateGameMasterRequest extends FormRequest
         return [
             'location' => 'required|string|max:100',
             'game_description' => 'required|string|max:1000',
-            'max_players' => 'required|integer|min:1',
+            'max_players' => 'required|integer|min:1|max:127',
             'profile_img' => 'nullable|file|mimes:jpeg,png,jpg|max:2048',
             'game_systems' => 'required|array|min:1',
             'game_systems.*' => 'exists:game_systems,id',
