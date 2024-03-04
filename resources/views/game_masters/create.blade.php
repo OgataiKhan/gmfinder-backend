164370b112ef536d9e58c3e63de468ad753ee190
dieromeo <diegoromeo99@icloud.com>
2024-03-04 15:19:10 +0100

    feat: create validation

--- resources/views/game_masters/create.blade.php
+++ resources/views/game_masters/create.blade.php
@@ -11,7 +11,8 @@
                 {{-- Location --}}
                 <div class="col-6 mb-3">
                     <label for="location" class="form-label">Location</label>
-                    <select class="form-select {{ $errors->has('location') ? 'is-invalid' : '' }}" name="location" id="location">
+                    <select class="form-select {{ $errors->has('location') ? 'is-invalid' : '' }}" name="location"
+                        id="location" required>
                         <option value="" disabled selected hidden>Select a province</option>
                         @foreach ($province as $single_province)
                             <option value="{{ $single_province }}" @if (old('location') == $single_province) selected @endif>
@@ -27,8 +28,8 @@
                 {{-- Max Players --}}
                 <div class="mb-3 col-6 ms-1">
                     <label for="max_players" class="form-label">Max Players</label>
-                    <input type="number" class="form-control {{ $errors->has('max_players') ? 'is-invalid' : '' }}" id="max_players" name="max_players"
-                        value="{{ old('max_players') }}">
+                    <input type="number" class="form-control {{ $errors->has('max_players') ? 'is-invalid' : '' }}"
+                        id="max_players" name="max_players" value="{{ old('max_players') }}" required min="1">
                     @if ($errors->has('max_players'))
                         <div class="invalid-feedback">
                             {{ $errors->first('max_players') }}
@@ -39,7 +40,8 @@
             {{-- Game Description --}}
             <div class="mb-3">
                 <label for="game_description" class="form-label">Game description</label>
-                <textarea class="form-control {{ $errors->has('game_description') ? 'is-invalid' : '' }}" name="game_description" id="game_description" rows="3">{{ old('game_description') }}</textarea>
+                <textarea class="form-control {{ $errors->has('game_description') ? 'is-invalid' : '' }}" name="game_description"
+                    id="game_description" rows="3" required maxlength="1000">{{ old('game_description') }}</textarea>
                 @if ($errors->has('game_description'))
                     <div class="invalid-feedback">
                         {{ $errors->first('game_description') }}
@@ -67,7 +69,8 @@
             {{-- Profile Picture --}}
             <div class="mb-3 col-12">
                 <label for="profile_img" class="form-label">Choose a profile picture</label>
-                <input class="form-control {{ $errors->has('profile_img') ? 'is-invalid' : '' }}" type="file" id="profile_img" name="profile_img">
+                <input class="form-control {{ $errors->has('profile_img') ? 'is-invalid' : '' }}" type="file"
+                    maxlength="2048" id="profile_img" name="profile_img">
                 @if ($errors->has('profile_img'))
                     <div class="invalid-feedback">
                         {{ $errors->first('profile_img') }}
