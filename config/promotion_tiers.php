a93d847907d30d71d711f24b319b67012fefe075
OgataiKhan <badellino.kevin@gmail.com>
2024-03-10 13:01:33 +0100

    feat: add promotions price

--- config/promotion_tiers.php
+++ config/promotion_tiers.php
@@ -6,17 +6,20 @@ return [
     "hours" => 24,
     "message" => '1 Day',
     "img" => 'bronze-img.webp',
+    "price (EUR)" => 2.99,
   ],
   [
     "tier" => "silver",
     "hours" => 72,
     "message" => '3 Days',
     "img" => 'silver-img.webp',
+    "price (EUR)" => 5.99,
   ],
   [
     "tier" => "gold",
     "hours" => 144,
     "message" => '6 Days',
     "img" => 'gold-img.webp',
+    "price (EUR)" => 9.99,
   ],
 ];
