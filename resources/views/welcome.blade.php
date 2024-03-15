699f077c533681bd5f0f17797f3e199573d01232
OgataiKhan <badellino.kevin@gmail.com>
2024-03-15 09:20:57 +0100

    fix: start page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,6 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>{{ config('app.name', 'GM Finder') }}</title>
+    <link rel="icon" href="{{ asset('img/favicon.png') }}">
 
     <!-- Fontawesome 6 cdn -->
     <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css'
@@ -32,11 +33,11 @@
             <div>
                 <div class="container flex-column d-flex align-items-center my-5">
                     <a href="{{ env('FRONTEND_URL') }}"><img class="img" src="./img/logo.png" alt="welcome-logo"></a>
-                    <div class="container-title py-3 px-4 mt-3">
+                    {{-- <div class="container-title py-3 px-4 mt-3">
                         <h2 class="fw-bold text-center text-black">
                             Welcome to GM Finder
                         </h2>
-                    </div>
+                    </div> --}}
                 </div>
             </div>
         </div>
